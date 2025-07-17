de0f3ea02634b603c1204f71ac6c4178e56cdb5f
LoginPage <rahulshakya7685@gmail.com>
2025-07-17 21:21:11 +0530

    Marquee Set up complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import Hero from './sections/Hero'
 import ShowCaseSection from './sections/ShowCaseSection'
+import Navbar from './components/Navbar'
+import LogoSection from './sections/LogoSection'
+import FeatureCards from './sections/FeatureCards'
 
 const App = () => {
   return (
     <>
-    
+    <Navbar />
     <Hero />
     <ShowCaseSection />
+    <LogoSection />
+    <FeatureCards />
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { navLinks } from '../constants'
 
 const Navbar = () => {
+    const [scrolled, setscrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const isScrolled = window.scrollY > 10
+            setscrolled(true)
+        }
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll );
+    })
+
   return (
-    <div>Navbar</div>
+    <header className={`navbar ${scrolled ? 'scrolled' : "not-scrolled"}`}>
+        <div className='inner'>
+            <a className='logo' href="#hero">
+                Rahul | JSM
+            </a>
+            <nav className='desktop'>
+                <ul>
+                    {
+                        navLinks?.map(({link, name}) => (
+                            <li key={name} className='group'>
+                                <a href={link}>
+                                    <span>{name}</span>
+                                    <span className="underline" />
+                                </a>
+                                
+
+                            </li>
+                        ))
+                    }
+                </ul>
+            </nav>
+            <a href='#contact' className='contact-btn group'>
+                <div className='inner'>
+                    <span>Contact Me</span>
+
+                </div>
+
+            </a>
+
+        </div>
+
+    </header>
   )
 }
 
